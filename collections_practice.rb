ae6b7b06efd3692a0f9c74aa82e489c8c71049db
norman lewis <takialog@gmail.com>
2018-02-28 15:31:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,5 +39,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|word, i|  i == 1 ? word << "s" : word }
+  array.each_with_index.collect{|word, i|  i != 1 ? word << "s" : word }
 end
